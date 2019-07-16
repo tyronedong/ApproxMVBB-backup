1f42b32241b94e3fff5de92d2bd2af74724eea1b
tyronedong <dtz1994@outlook.com>
2019-07-16 13:04:03 +0800

    success build instance

---
+++ build/thirdparty/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Github/ApproxMVBB/build/thirdparty/googletest-src/googlemock
+# Build directory: D:/Github/ApproxMVBB/build/thirdparty/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
